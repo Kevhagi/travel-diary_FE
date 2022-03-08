4ee80373c9eb5fc21f433185d33f9685c6eb88c2
Kevhagi <94234971+Kevhagi@users.noreply.github.com>
2022-03-08 20:08:05 +0700

    WIP homeBody

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React, { useEffect, useContext }from 'react'
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom"
 import './App.css'
-import 'bootstrap/dist/css/bootstrap.min.css';
 
 import Home from './pages/Home';
 import Profile from './pages/Profile'
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,29 @@
 import React from 'react'
 import NavigationBar from '../components/Navbar'
+import { Button } from 'react-bootstrap'
+import '../css/Home.css'
 
 function Home() {
     return (
-        <NavigationBar />
+        <div>
+            <NavigationBar />
+            <div className='homeBody p-5'>
+                <h2 className='title fs-1'>Journey</h2>
+
+                <div className='searchbar input-group px-5 py-3'>
+                    <input 
+                        type="text" 
+                        class="form-control" 
+                        placeholder="Find Journey" 
+                        aria-label="Recipient's username" 
+                        aria-describedby="searchbar" 
+                    />
+                    <Button className='px-4 input-group-text' variant="primary fw-bold" id="searchbar">Search</Button>  
+                </div>
+                                
+                <p>Cards</p>
+            </div>
+        </div>
     )
 }
 
