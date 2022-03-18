49910daae62326f99c92108ae4cccb3e616e5a39
Kevhagi <94234971+Kevhagi@users.noreply.github.com>
2022-03-18 15:50:56 +0700

    Fix styling circular progress

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -258,6 +258,13 @@ function Profile() {
                             {loading && (
                                 <CircularProgress
                                     size={24}
+                                    sx={{
+                                    position: 'absolute',
+                                    top: '50%',
+                                    left: '50%',
+                                    marginTop: '-12px',
+                                    marginLeft: '-12px',
+                                  }}
                                 />
                             )}
                         </Box>
